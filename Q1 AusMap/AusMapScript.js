d8db078f3d4157d2e9494014e2332dd26d19f640
102099233 <100542070+102099233@users.noreply.github.com>
2022-05-14 14:38:00 +1000

    Created tooltip for data to show from file

--- Q1 AusMap/AusMapScript.js
+++ Q1 AusMap/AusMapScript.js
@@ -1,36 +1,60 @@
-var w = 500;
-var h = 450;
+var w = 850;
+var h = 700;
 
 var projection = d3.geoMercator()
                     .center([135, -30])
                     .translate([w/2, h/2])
-                    .scale(500);
+                    .scale(1000);
 
 var path = d3.geoPath()
               .projection(projection);
 
+var color = d3.scaleOrdinal()
+              .range(['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9']);
+
 var svg = d3.select("#chart")
             .append("svg")
             .attr("width", w)
-            .attr("height", h)
-            .attr("fill", "grey")
-            .on("mouseover", function(event, d) {
-                d3.select(this)
-                  .attr("fill", "orange");})
-            .on("mouseout", function() {
-              d3.select("#tooltip").remove();//removes the tooltip that was previously created in mouseover
-              d3.select(this)
-                .attr("fill", "slategrey");});
+            .attr("height", h);
 
-var xScale = d3.scaleBand()
-              .domain([0,h])
-              .rangeRound([0,w]);
 
-//d3.csv("VIC_LGA_unemployment.csv").then( function(data) {
+d3.csv("Question 1 Map.csv").then( function(data) {
 d3.json("aust.json").then(function(json) {
+  var dataSTATE_NAME = new Array();
+  var dataTRenew = new Array();
+  var dataTNonRenew = new Array();
+  var jsonSTATE_NAME = new Array();
+  for (var i=0; i < data.length; i++) {
+    dataSTATE_NAME[i] = data[i].STATE_NAME;
+    dataTRenew[i] = parseFloat(data[i].TRenew);
+    dataTNonRenew[i] = parseFloat(data[i].TNonRenew);
+    for(var j = 0; j < json.features.length; j++) {
+      jsonSTATE_NAME[j] = json.features[j].properties.STATE_NAME;
+    }
+  }
+console.table(dataSTATE_NAME);
   svg.selectAll("path")
       .data(json.features)
       .enter()
       .append("path")
-      .attr("d", path);
+      .attr("d", path)
+      .attr("stroke", "dimgray")
+      .attr("fill", function(d, i) {return color(i)})
+      .append("title")
+      .attr("fill", "darkslategray")
+      .text(function(d, i) {
+          if(dataSTATE_NAME[i] == jsonSTATE_NAME[i])
+          {
+            if(dataTRenew[i] == 0 && dataTNonRenew[i] == 0)
+            {
+              return "Total Renewable: Data Not Available" + "\n" + "Total Non Renewable: Data Not Available";
+            }
+            else
+            {
+              return "Total Renewable: " + dataTRenew[i] + "\n" + "Total Non Renewable: " + dataTNonRenew[i];
+            }
+          }
+      });
+
+    })
 })
