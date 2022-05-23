279c5978f77c9a49a94d48f0b02e4d34c209a9f4
102099233 <100542070+102099233@users.noreply.github.com>
2022-05-23 11:19:13 +1000

    Start of q1 Line Graph

---
+++ Q1 LineGraph/Q1_LineGraph.js
@@ -0,0 +1,46 @@
+function nonRenew() {
+	var w = 600;
+	var h = 300;
+
+	var dataset;
+
+	d3.csv("Q1 Line Non Renew CSV.csv", function(d) {
+		return {
+			date: new Date(+d.year, +d.month-1),
+			number: +d.number};}
+		, function(data) {
+						dataset = data;
+						//lineChart(dataset);
+						console.table(dataset, ["date", "number"]);
+					});
+
+ xScale = d3.scaleTime()
+									.domain([d3.min(dataset, function(d) {return d.date;}), d3.max(dataset, function(d) {return d.date;})])
+									.range([0, w]);
+
+
+
+yScale = d3.scaleLinear()
+									.domain([0, d3.max(dataset, function(d) { return d.number; })])
+									.range ([h, 0]);
+
+ var line = d3.line()
+ 							.x(function(d) { return xScale(d.date); })
+							.y(function(d) { return yScale(d.average); });
+
+
+
+	var svg = d3.select("#chart")
+				.append("svg")
+				.attr("width", w)
+				.attr("height", h);
+
+			svg.append("path")
+			.datum(dataset)
+			.attr("class", "line")
+			.attr("d", line);
+}
+
+function renew() {
+
+}
