64c1327b60cfb71a6335d79466b7f7c68877fcf4
102099233 <100542070+102099233@users.noreply.github.com>
2022-06-01 13:09:38 +1000

    Added basic tooltip, added comments. Simple design edits

--- Q3_MutiLineGraph/Q3 Line.js
+++ Q3_MutiLineGraph/Q3 Line.js
@@ -1,11 +1,11 @@
-
+//Multi line chart
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
-
+//setting x and y scales
 var x = d3.scalePoint().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
-
+//creating each line based on column heading
 var valueline1 = d3.line()
 		    					.x(function(d) { return x(d.Years); })
 		    					.y(function(d) { return y(d.Agriculture); });
@@ -35,7 +35,7 @@ var valueline9 = d3.line()
 									.y(function(d) { return y(d.Residential); });
 
 
-
+//create svg for chart and legend
 var svg = d3.select("#chart")
 						.append("svg")
 						.attr("width", width + margin.left + margin.right + 20)
@@ -49,13 +49,14 @@ var svg2 = d3.select("#chart")
 						.attr("height", 270)
 						.append("g");
 
-
+//calling data source
 d3.csv("Question 3 Line Chart CSV.csv").then(function(data) {
+  //storing data in string or numerical form
 					data.forEach(function(d) {
 						    d.Years = d.Years;
 						    d.Agriculture = +d.Agriculture;
 						    d.Mining = +d.Mining;
-								d.Manufacturing = d.Manufacturing;
+								d.Manufacturing = +d.Manufacturing;
 						    d.ElectricityGeneration = +d.ElectricityGeneration;
 								d.Construction = +d.Construction;
 						    d.Transport = +d.Transport;
@@ -63,85 +64,296 @@ d3.csv("Question 3 Line Chart CSV.csv").then(function(data) {
 								d.CommercialServices = +d.CommercialServices;
 								d.Residential = +d.Residential;
 						  	});
+//creating tooltip variable
+                var Tooltip = d3.select("#chart")
+                                .append("div")
+                                .attr("class", "tooltip")
+                                .style("opacity", 0)
+                                .style("background-color", "white")
+                                .style("border", "solid")
+                                .style("border-width", "2px")
+                                .style("border-radius", "5px")
+                                .style("padding", "5px")
+                                .style("position", "absolute");
 
-
-
+//setting domain based on values of data
 					x.domain(d3.map(data, function(d) { return d.Years; }));
 					y.domain([0, d3.max(data, function(d) {
 	  					return Math.max(d.Agriculture, d.Mining, d.Manufacturing, d.ElectricityGeneration, d.Construction, d.Transport, d.WaterWaste, d.CommercialServices, d.Residential); })]);
 
+//appending paths for each line include on mouse functions
+
+//Value Line 1
 					svg.append("path")
 	       			.data([data])
 	       			.attr("class", "line")
-							.style("stroke", d3.schemeCategory10[0])
-							.attr("stroke-width", 2)
+							.style("stroke", d3.schemeCategory10[0]) //giving colour to line
+							.attr("stroke-width", 4)
 							.style("fill","none")
-	       			.attr("d", valueline1);
+              .style("cursor", "pointer")//setting cursor to pointer
+	       			.attr("d", valueline1)
+              .on("mouseover", function (event, d) {
+                d3.select(this)
+                  .transition()
+                  .duration("50")
+                  .attr("opacity", ".50");
+                  Tooltip.style("opacity", 1);
 
+              })
+              .on("mousemove", function(event, d) {
+                  Tooltip.html(keys[0] + " Energy Consumption")
+                         .style("left", (d3.pointer(event)[0]) + "px")
+                         .style("top", (d3.pointer(event)[1]) + "px");
+                })
+              .on("mouseout", function (event, d) {
+                  d3.select(this)
+                    .transition()
+                    .duration("50")
+                    .attr("opacity", "1");
+                    Tooltip.style("opacity", 0);
+              });
+
+//Value Line 2
 							svg.append("path")
 							.data([data])
 							.attr("class", "line")
 							.style("stroke", d3.schemeCategory10[1])
-							.attr("stroke-width", 2)
+							.attr("stroke-width", 4)
 							.style("fill","none")
-							.attr("d", valueline2);
+              .style("cursor", "pointer")
+							.attr("d", valueline2)
+              .on("mouseover", function (event, d) {
+                d3.select(this)
+                  .transition()
+                  .duration("50")
+                  .attr("opacity", ".50");
+                  Tooltip.style("opacity", 1);
 
+              })
+              .on("mousemove", function(event, d) {
+                  Tooltip.html(keys[1] + " Energy Consumption")
+                         .style("left", (d3.pointer(event)[0]) + "px")
+                         .style("top", (d3.pointer(event)[1]) + "px");
+                })
+              .on("mouseout", function (event, d) {
+                  d3.select(this)
+                    .transition()
+                    .duration("50")
+                    .attr("opacity", "1");
+                    Tooltip.style("opacity", 0);
+              });
+
+//Value Line 3
 							svg.append("path")
 							.data([data])
 							.attr("class", "line")
 							.style("stroke", d3.schemeCategory10[2])
-							.attr("stroke-width", 2)
+							.attr("stroke-width", 4)
 							.style("fill","none")
-							.attr("d", valueline3);
+              .style("cursor", "pointer")
+							.attr("d", valueline3)
+              .on("mouseover", function (event, d) {
+                d3.select(this)
+                  .transition()
+                  .duration("50")
+                  .attr("opacity", ".50");
+                  Tooltip.style("opacity", 1);
 
+              })
+              .on("mousemove", function(event, d) {
+                  Tooltip.html(keys[2] + " Energy Consumption")
+                         .style("left", (d3.pointer(event)[0]) + "px")
+                         .style("top", (d3.pointer(event)[1]) + "px");
+                })
+              .on("mouseout", function (event, d) {
+                  d3.select(this)
+                    .transition()
+                    .duration("50")
+                    .attr("opacity", "1");
+                    Tooltip.style("opacity", 0);
+              });
+
+//Value Line 4
 							svg.append("path")
 							.data([data])
 							.attr("class", "line")
 							.style("stroke", d3.schemeCategory10[3])
-							.attr("stroke-width", 2)
+							.attr("stroke-width", 4)
 							.style("fill","none")
-							.attr("d", valueline4);
+              .style("cursor", "pointer")
+							.attr("d", valueline4)
+              .on("mouseover", function (event, d) {
+                d3.select(this)
+                  .transition()
+                  .duration("50")
+                  .attr("opacity", ".50");
+                  Tooltip.style("opacity", 1);
 
+              })
+              .on("mousemove", function(event, d) {
+                  Tooltip.html(keys[3] + " Energy Consumption")
+                         .style("left", (d3.pointer(event)[0]) + "px")
+                         .style("top", (d3.pointer(event)[1]) + "px");
+                })
+              .on("mouseout", function (event, d) {
+                  d3.select(this)
+                    .transition()
+                    .duration("50")
+                    .attr("opacity", "1");
+                    Tooltip.style("opacity", 0);
+              });
+
+//Value Line 5
 							svg.append("path")
 							.data([data])
 							.attr("class", "line")
 							.style("stroke", d3.schemeCategory10[4])
-							.attr("stroke-width", 2)
+							.attr("stroke-width", 4)
 							.style("fill","none")
-							.attr("d", valueline5);
+              .style("cursor", "pointer")
+							.attr("d", valueline5)
+              .on("mouseover", function (event, d) {
+                d3.select(this)
+                  .transition()
+                  .duration("50")
+                  .attr("opacity", ".50");
+                  Tooltip.style("opacity", 1);
 
+              })
+              .on("mousemove", function(event, d) {
+                  Tooltip.html(keys[4] + " Energy Consumption")
+                         .style("left", (d3.pointer(event)[0]) + "px")
+                         .style("top", (d3.pointer(event)[1]) + "px");
+                })
+              .on("mouseout", function (event, d) {
+                  d3.select(this)
+                    .transition()
+                    .duration("50")
+                    .attr("opacity", "1");
+                    Tooltip.style("opacity", 0);
+              });
+
+//Value Line 6
 							svg.append("path")
 							.data([data])
 							.attr("class", "line")
 							.style("stroke", d3.schemeCategory10[5])
-							.attr("stroke-width", 2)
+							.attr("stroke-width", 4)
 							.style("fill","none")
-							.attr("d", valueline6);
+              .style("cursor", "pointer")
+							.attr("d", valueline6)
+              .on("mouseover", function (event, d) {
+                d3.select(this)
+                  .transition()
+                  .duration("50")
+                  .attr("opacity", ".50");
+                  Tooltip.style("opacity", 1);
 
+              })
+              .on("mousemove", function(event, d) {
+                  Tooltip.html(keys[5] + " Energy Consumption")
+                         .style("left", (d3.pointer(event)[0]) + "px")
+                         .style("top", (d3.pointer(event)[1]) + "px");
+                })
+              .on("mouseout", function (event, d) {
+                  d3.select(this)
+                    .transition()
+                    .duration("50")
+                    .attr("opacity", "1");
+                    Tooltip.style("opacity", 0);
+              });
+
+//Value Line 7
 							svg.append("path")
 							.data([data])
 							.attr("class", "line")
 							.style("stroke", d3.schemeCategory10[6])
-							.attr("stroke-width", 2)
+							.attr("stroke-width", 4)
 							.style("fill","none")
-							.attr("d", valueline7);
+              .style("cursor", "pointer")
+							.attr("d", valueline7)
+              .on("mouseover", function (event, d) {
+                d3.select(this)
+                  .transition()
+                  .duration("50")
+                  .attr("opacity", ".50");
+                  Tooltip.style("opacity", 1);
 
+              })
+              .on("mousemove", function(event, d) {
+                  Tooltip.html(keys[6] + " Energy Consumption")
+                         .style("left", (d3.pointer(event)[0]) + "px")
+                         .style("top", (d3.pointer(event)[1]) + "px");
+                })
+              .on("mouseout", function (event, d) {
+                  d3.select(this)
+                    .transition()
+                    .duration("50")
+                    .attr("opacity", "1");
+                    Tooltip.style("opacity", 0);
+              });
+
+//Value Line 8
 							svg.append("path")
 							.data([data])
 							.attr("class", "line")
 							.style("stroke", d3.schemeCategory10[7])
-							.attr("stroke-width", 2)
+							.attr("stroke-width", 4)
 							.style("fill","none")
-							.attr("d", valueline8);
+              .style("cursor", "pointer")
+							.attr("d", valueline8)
+              .on("mouseover", function (event, d) {
+                d3.select(this)
+                  .transition()
+                  .duration("50")
+                  .attr("opacity", ".50");
+                  Tooltip.style("opacity", 1);
 
+              })
+              .on("mousemove", function(event, d) {
+                  Tooltip.html(keys[7] + " Energy Consumption")
+                         .style("left", (d3.pointer(event)[0]) + "px")
+                         .style("top", (d3.pointer(event)[1]) + "px");
+                })
+              .on("mouseout", function (event, d) {
+                  d3.select(this)
+                    .transition()
+                    .duration("50")
+                    .attr("opacity", "1");
+                    Tooltip.style("opacity", 0);
+              });
+
+//Value Line 9
 							svg.append("path")
 							.data([data])
 							.attr("class", "line")
 							.style("stroke", d3.schemeCategory10[8])
-							.attr("stroke-width", 2)
+							.attr("stroke-width", 4)
 							.style("fill","none")
-							.attr("d", valueline9);
+              .style("cursor", "pointer")
+							.attr("d", valueline9)
+              .on("mouseover", function (event, d) {
+                d3.select(this)
+                  .transition()
+                  .duration("50")
+                  .attr("opacity", ".50");
+                  Tooltip.style("opacity", 1);
 
+              })
+              .on("mousemove", function(event, d) {
+                  Tooltip.html(keys[8] + " Energy Consumption")
+                         .style("left", (d3.pointer(event)[0]) + "px")
+                         .style("top", (d3.pointer(event)[1]) + "px");
+                })
+              .on("mouseout", function (event, d) {
+                  d3.select(this)
+                    .transition()
+                    .duration("50")
+                    .attr("opacity", "1");
+                    Tooltip.style("opacity", 0);
+              });
+
+//appending axis to chart
 	   			svg.append("g")
 	       			.attr("transform", "translate(0," + height + ")")
 	       			.call(d3.axisBottom(x));
@@ -149,6 +361,7 @@ d3.csv("Question 3 Line Chart CSV.csv").then(function(data) {
 	   			svg.append("g")
 	       			.call(d3.axisLeft(y));
 
+//Axis labels
               svg.append("text")
                   .attr("text-anchor", "end")
                   .attr("x", width/2)
@@ -162,6 +375,7 @@ d3.csv("Question 3 Line Chart CSV.csv").then(function(data) {
                   .attr("x", -(height/2))
                   .text("Net Energy Consumption (PJ)");
 
+//Appending background grid to chart
 					svg.append('g')
 							.attr('class', 'grid')
 							.attr('transform', 'translate(0,' + height + ')')
@@ -171,13 +385,14 @@ d3.csv("Question 3 Line Chart CSV.csv").then(function(data) {
 							.attr('class', 'grid')
 							.call(d3.axisLeft(y).tickSize(-width).tickFormat(''));
 
-
+//creating keys for the legend and settign colour to each one
 							var keys = ["Agriculture", "Mining", "Manufacturing", "Electricity Generation", "Construction", "Transport", "Water Waste", "Commercial Services", "Residential"];
 
 							var color = d3.scaleOrdinal()
 								.domain(keys)
 								.range(d3.schemeCategory10);
 
+//Placing dots on the legend
 							svg2.selectAll("mydots")
 								.data(keys)
 								.enter()
@@ -187,6 +402,7 @@ d3.csv("Question 3 Line Chart CSV.csv").then(function(data) {
 								.attr("r", 7)
 								.style("fill", function(d){ return color(d)});
 
+//Placing key values on the legend
 							svg2.selectAll("mylabels")
 								.data(keys)
 								.enter()
